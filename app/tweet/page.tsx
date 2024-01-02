b400720051470d3b87e3363847a0e0dd6d534f56
alouilouai <louaialoui1993@gmail.com>
2024-01-03 00:52:47 +0100

    more design

--- app/tweet/page.tsx
+++ app/tweet/page.tsx
@@ -4,6 +4,7 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Session } from "@supabase/supabase-js";
 import Likes from "../likes/page";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 interface TweetsProps {
   session: Session;
@@ -60,13 +61,30 @@ export default function Tweets({ session }: TweetsProps) {
   return (
     <div>
       {tweets?.map((tweet) => (
-        <div key={tweet.id}>
+        <div
+        key={tweet.id}
+        className="border border-gray-800 border-t-0 px-4 py-8 flex"
+      >
+        <div className="h-12 w-12">
+          <Image
+            className="rounded-full"
+            src={tweet.author.avatar_url}
+            alt="tweet user avatar"
+            width={48}
+            height={48}
+          />
+        </div>
+        <div className="ml-4">
           <p>
-            {tweet?.author?.name} {tweet?.author?.username}
+            <span className="font-bold">{tweet.author.name}</span>
+            <span className="text-sm ml-2 text-gray-400">
+              {tweet.author.username}
+            </span>
           </p>
           <p>{tweet.title}</p>
           <Likes tweet={tweet} />
         </div>
+      </div>
       ))}
     </div>
   );
